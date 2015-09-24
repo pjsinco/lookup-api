5bc74f7f939cd7e7eb47caa399dfedf9c3879449
pjsinco <psinco@fas.harvard.edu>
2015-09-24 06:16:13 -0500

    Set up models

--- app/Location.php
+++ app/Location.php
@@ -2,9 +2,22 @@
 
 namespace App;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    //
+    public $timestamps = false;
+
+
+    public static function random()
+    {
+        $location = DB::table('locations')
+            ->orderByRaw('RAND()')
+            ->first();
+        return $location;
+    }
+
+
 }
+
--- app/Physician.php
+++ app/Physician.php
@@ -6,5 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Physician extends Model
 {
-    //
+    //public $timestamps = false;
+    //protected $primaryKey = 'aoa_mem_id';
+
+    protected $fillable = [
+        'prefix',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'suffix',
+        'designation',
+        'City',
+        'State_Province',
+        'Zip',
+        'Country',
+        'PrimaryPracticeFocusCode',
+        'PrimaryPracticeFocusArea',
+        'SecondaryPracticeFocusCode',
+        'SecondaryPracticeFocusArea',
+        'website',
+        'address_1',
+        'address_2',
+        'Phone',
+        'Email',
+    ];
+
+    public $hidden = [
+        'id',
+        'aoa_mem_id',
+    ];
 }
+
+
+
