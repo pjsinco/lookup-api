1d02897658e1dcf83b3026d67e258b7485b3c3c2
pjsinco <psinco@fas.harvard.edu>
2015-10-23 14:24:43 -0500

    Complete a successful refresh with new RefreshController@refresh

--- app/Http/Controllers/RefreshController.php
+++ app/Http/Controllers/RefreshController.php
@@ -11,47 +11,6 @@ use Stanley\Geocodio\Client;
 
 class RefreshController extends Controller
 {
-    /**
-     * Fields in the iMIS FindYourDO table
-     *
-     */
-    private $iMisHeaders = [
-        'id',
-        'full_name',
-        'prefix',
-        'first_name',
-        'middle_name',
-        'last_name',
-        'suffix',
-        'designation',
-        'SortColumn',
-        'MemberStatus',
-        'City',
-        'State_Province',
-        'Zip',
-        'Country',
-        'COLLEGE_CODE',
-        'YearOfGraduation',
-        'fellows',
-        'PrimaryPracticeFocusCode',
-        'PrimaryPracticeFocusArea',
-        'SecondaryPracticeFocusCode',
-        'SecondaryPracticeFocusArea',
-        'website',
-        'AOABoardCertified',
-        'address_1',
-        'address_2',
-        'Phone',
-        'Email',
-        'ABMS',
-        'Gender',
-        'CERT1',
-        'CERT2',
-        'CERT3',
-        'CERT4',
-        'CERT5'
-    ];
-
     private $tempLocationTable = 'temp_locations';
     private $countGeoLocated = 0;
     private $countImisRaw = 0;
@@ -71,12 +30,17 @@ class RefreshController extends Controller
         $this->createPhysicianModels();
 
         $query = $this->dropTempLocationTable();
+
+        Log::info('Geolocoated ' . $this->countGeoLocated . ' physicians');
     }
 
     private function parseGeoData($geoDataRaw)
     {
-dd($geoDataRaw);
-        $data = new \StdClass();
+        if (count($geoDataRaw->response->results) == 0) {
+            return false;
+        } 
+
+        $data = new \StdClass(); 
         $data->lat = $geoDataRaw->response->results[0]->location->lat;
         $data->lon = $geoDataRaw->response->results[0]->location->lng;
         $data->geo_city = 
@@ -91,6 +55,7 @@ dd($geoDataRaw);
 
     private function geolocate($physician)
     {
+        $this->countGeolocated++;
         Log::notice('Fetching geolocation data for ' . $physician->full_name);
 
         $client = new Client('aca4585cbbdf2f8589c58bb4606c56fd45db3bb');
@@ -126,7 +91,6 @@ dd($geoDataRaw);
 
     private function getPhysicianLocationData($physician)
     {
-
         $geoData = DB::selectOne("select *
                 from $this->tempLocationTable
                 where address_1 = :address
@@ -140,16 +104,6 @@ dd($geoDataRaw);
                 'zip' => $physician->Zip,
             ]
         );
-//        $q = "
-//            select * 
-//            from $this->tempLocationTable
-//            where address_1 = \"$physician->address_1\"
-//                and City = \"$physician->City\" 
-//                and State_Province = \"$physician->State_Province\"
-//                and Zip = \"$physician->Zip\"
-//        ";
-//
-//        $geoData = DB::selectOne($q);
 
         if (empty($geoData)) {
             $geoData = $this->geolocate($physician);
@@ -167,6 +121,11 @@ dd($geoDataRaw);
         foreach ($rows as $row) {
 
             $locationData = $this->getPhysicianLocationData($row);
+
+            if (!$locationData) { 
+                Log::error('Could not geolocate ' . $row->full_name);
+                continue; 
+            }
 
             $physician = \App\Physician::create([
                 'aoa_mem_id'                 => $row->id,
@@ -211,7 +170,6 @@ dd($geoDataRaw);
                 'geo_matches'                => $locationData->geo_matches,
             ]);
         }
-        
     }
 
     private function populateTempLocationTable()
@@ -236,7 +194,6 @@ dd($geoDataRaw);
 
     private function createTempLocationTable()
     {
-        
         $this->dropTempLocationTable();
 
         $q = "
@@ -320,44 +277,4 @@ dd($geoDataRaw);
     {
 
     }
-    
-//    private function addPhysician($physician)
-//    {
-//        echo 'We\'re going to add this physician: ';
-//        echo '<pre>'; echo $physician['full_name']; echo '</pre>'; ;
-//    }
-
-//    private function getCurrentPhysician($id)
-//    {
-//        return \App\Physician::where('aoa_mem_id', '=', $id)->first();
-//    }
-
-//    private function getPdoStatementForImisRows()
-//    {
-//        $user = env('MSSQL_USERNAME');
-//        $password = env('MSSQL_PASSWORD');
-//
-//        $db = new \PDO(
-//            'dblib:host=sql05-1.aoanet.local;dbname=imis', 
-//            $user, 
-//            $password
-//        );
-//
-//        $q = "
-//            SELECT * 
-//            FROM imis.dbo.vFindYourDO 
-//            WHERE country = 'USA'
-//                and not (first_name like '%test%' or 
-//                    first_name like '%AOA%'
-//                )
-//            ORDER BY id
-//        ";
-//        //$stmt = $db->query($q);
-//        $stmt = $db->prepare($q);
-//        $stmt->execute();
-//
-//        return $stmt;
-//    }
-
-
 }
