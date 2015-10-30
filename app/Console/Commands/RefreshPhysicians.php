63a19bb94d982a01eef236b5270b01fe4822a15c
pjsinco <psinco@fas.harvard.edu>
2015-10-30 07:00:54 -0500

    Add class RefreshFromImis

--- app/Console/Commands/RefreshPhysicians.php
+++ app/Console/Commands/RefreshPhysicians.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use DB;
 use Log;
 use Stanley\Geocodio\Client;
+use Elit\RefreshFromImis;
 
 class RefreshPhysicians extends Command
 {
@@ -40,50 +41,6 @@ class RefreshPhysicians extends Command
      */
     public function handle()
     {
-        DB::table('imis_raw')->truncate();
-
-        $user = env('MSSQL_USERNAME');
-        $password = env('MSSQL_PASSWORD');
-
-        try {
-            $db = new \PDO(
-                'dblib:host=sql05-1.aoanet.local;dbname=imis', 
-                $user, 
-                $password
-            );
-
-            $q = "
-                SELECT * 
-                FROM imis.dbo.vFindYourDO 
-                WHERE country = 'USA'
-                    and not (first_name like '%test%' or 
-                        first_name like '%AOA%'
-                    )
-                ORDER BY id
-            ";
-
-            $stmt = $db->prepare($q);
-
-            if ($stmt) {
-                $stmt->execute();
-            }
-    
-            $rowCount = 0;
-
-            while (($row = $stmt->fetch(\PDO::FETCH_ASSOC)) !== false) {
-                $rowCount++;
-                DB::table('imis_raw')
-                    ->insert($row);
-            }
-            
-            $msg = sprintf('Refreshing imis_raw table: %d rows', $rowCount);
-
-            Log::info($msg);
-            $this->info($msg);
-
-        } catch (\PDOException $e) {
-            Log::error($e->getMessage());
-            $this->error($e->getMessage());
-        }
+        RefreshFromImis::createImisTable();
     }
 }
---
+++ app/Elit/RefreshFromImis.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Elit\RefreshFromImis;
+
+use DB;
+use Log;
+
+/**
+ * Update Find Your DO data with data from iMis.
+ */
+class RefreshFromImis
+{
+    
+    /**
+     * 
+     */
+    public function __construct()
+    {
+        
+    }
+
+    public static function createImisTable()
+    {
+        DB::table('imis_raw')->truncate();
+
+        $user = env('MSSQL_USERNAME');
+        $password = env('MSSQL_PASSWORD');
+
+        try {
+            $db = new \PDO(
+                'dblib:host=sql05-1.aoanet.local;dbname=imis', 
+                $user, 
+                $password
+            );
+
+            $q = "
+                SELECT * 
+                FROM imis.dbo.vFindYourDO 
+                WHERE country = 'USA'
+                    and not (first_name like '%test%' or 
+                        first_name like '%AOA%'
+                    )
+                ORDER BY id
+            ";
+
+            $stmt = $db->prepare($q);
+
+            if ($stmt) {
+                $stmt->execute();
+echo '<pre>'; var_dump($stmt->rowCount()); echo '</pre>'; die();                 
+            }
+    
+            $rowCount = 0;
+
+            while (($row = $stmt->fetch(\PDO::FETCH_ASSOC)) !== false) {
+                $rowCount++;
+                DB::table('imis_raw')
+                    ->insert($row);
+            }
+            
+            $msg = sprintf('Refreshing imis_raw table: %d rows', $rowCount);
+
+            Log::info($msg);
+            $this->info($msg);
+
+        } catch (\PDOException $e) {
+            Log::error($e->getMessage());
+            $this->error($e->getMessage());
+        }
+    }
+}
