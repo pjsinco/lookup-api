ae4606ebfcec19aa5d99568c48961144ab27a5bb
pjsinco <psinco@gmail.com>
2016-02-03 09:12:09 -0600

    Fix count in queryMeta

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -59,15 +59,15 @@ class DoctorController extends Controller
       //while (empty($physicians) || $physicians->isEmpty()) {
       while (!$physicians || $physicians->count() == 0) {
         $searchDistance = $this->getNextDistance($searchDistance);
-  
+
         $physicians = Physician::withinRadius(
-          $coords['lat'], 
-          $coords['lon'], 
-          $searchDistance
-        )
-        ->alias($request->alias_id)
-        ->name($request->q) 
-        ->gender($request->gender);
+            $coords['lat'], 
+            $coords['lon'], 
+            $searchDistance
+            )
+          ->alias($request->alias_id)
+          ->name($request->q) 
+          ->gender($request->gender);
 
         if ($searchDistance == $this->maxDistance) {
           break;
@@ -77,13 +77,13 @@ class DoctorController extends Controller
     } else {
 
       $physicians = Physician::withinRadius(
-        $coords['lat'], 
-        $coords['lon'], 
-        $searchDistance
-      )
-      ->alias($request->alias_id)
-      ->name($request->q) 
-      ->gender($request->gender);
+          $coords['lat'], 
+          $coords['lon'], 
+          $searchDistance
+          )
+        ->alias($request->alias_id)
+        ->name($request->q) 
+        ->gender($request->gender);
     }
 
     $queryMeta = [
@@ -92,146 +92,147 @@ class DoctorController extends Controller
       'zip' => $request->zip ? 
         $request->zip : $this->getZip($request->city, $request->state),
       'alias_id' => $request->alias_id ? $request->alias_id : null,
+      'aggregate' => [ 'hello' => 'there', 'how' => 'are', 'you' => '?'],
       'q' => $request->q,
-      'count' => ($physicians ? count($physicians) : 0),
+      'count' => ($physicians ? $physicians->count() : 0),
       'radius' => $searchDistance
     ];
 
     if (!$physicians->get()->isEmpty()) {
       $physicians = $physicians->orderBy($orderBy, $sort)
-          ->paginate($limit)
-          ->appends($request->query());
+        ->paginate($limit)
+        ->appends($request->query());
 
       return $this->response->withPaginator(
-        $physicians,
-        new PhysicianTransformer,
-        null,
-        $queryMeta
-      );
+          $physicians,
+          new PhysicianTransformer,
+          null,
+          $queryMeta
+          );
     } 
-  
+
     $errorMeta = [
       'meta' => $queryMeta, 
       'error' => [
         'code' => 'GEN-NOT-FOUND',
-        'http_code' => 404,
-        'message' => 'Physician not found'
+      'http_code' => 404,
+      'message' => 'Physician not found'
       ]
     ]; 
-    return $this->response->withArray($errorMeta);
-  }
-
-  /**
-   * Show a physician
-   *
-   * @return void
-   * @author PJ
-   */
-  public function show($id)
-  {
-    $physician = Physician::find($id);
-    
-    if ($physician) {
-      return $this->response
-        ->withItem($physician, new PhysicianTransformer);
+      return $this->response->withArray($errorMeta);
     }
 
-    $errorMeta = [
-      'error' => [
-        'code' => 'GEN-NOT-FOUND',
-        'http_code' => 404,
-        'message' => 'Physician not found'
-      ]
-    ]; 
+    /**
+     * Show a physician
+     *
+     * @return void
+     * @author PJ
+     */
+    public function show($id)
+    {
+      $physician = Physician::find($id);
 
-    return $this->response->withArray($errorMeta);
-
-    //$resource = new Fractal\Resource\Item($phys, new PhysicianTransformer);
-    //$output = $this->manager->createData($resource)->toArray();
-
-    //return $output;
-
-
-    //return $this->respond([
-      //'data' => $this->physicianTransformer->transform($physician)
-    //]);
-  }
-
-  /**
-   * Get the next distance to try.
-   * Ex.: 33 is passed in, return 50.
-   * Ex.: 100 is passed in, return 250.
-   *
-   * @param int
-   * @return int
-   */
-  private function getNextDistance($queriedDistance = 0)
-  {
-    foreach ($this->fallbackDistances as $distance) {
-      if ($distance > $queriedDistance) {
-        return $distance;
+      if ($physician) {
+        return $this->response
+          ->withItem($physician, new PhysicianTransformer);
       }
+
+      $errorMeta = [
+        'error' => [
+          'code' => 'GEN-NOT-FOUND',
+        'http_code' => 404,
+        'message' => 'Physician not found'
+        ]
+      ]; 
+
+        return $this->response->withArray($errorMeta);
+
+        //$resource = new Fractal\Resource\Item($phys, new PhysicianTransformer);
+        //$output = $this->manager->createData($resource)->toArray();
+
+        //return $output;
+
+
+        //return $this->respond([
+        //'data' => $this->physicianTransformer->transform($physician)
+        //]);
     }
 
-    // the queriedDistance is already beyond our distances
-    return $queriedDistance;
-  }
+    /**
+     * Get the next distance to try.
+     * Ex.: 33 is passed in, return 50.
+     * Ex.: 100 is passed in, return 250.
+     *
+     * @param int
+     * @return int
+     */
+    private function getNextDistance($queriedDistance = 0)
+    {
+      foreach ($this->fallbackDistances as $distance) {
+        if ($distance > $queriedDistance) {
+          return $distance;
+        }
+      }
 
-  /**
-   * Get all aliases for a specialty.
-   *
-   * @param string $specialtyCode - the parent specialty code (ex.: 'AJI')
-   * @return array of IDs
-   *    ex.: [80, 5, 35]
-   */
-  private function getAliases($specialtyCode)
-  {
-    $aliasIds = DB::Table('specialty_alias')
-      ->addSelect('alias_id')
-      ->where('specialty_id', '=', $specialty)->get();
-  }
+      // the queriedDistance is already beyond our distances
+      return $queriedDistance;
+    }
 
-  /**
-   * Find coordinates for a location, based on zip or city and state.
-   *
-   */
-  public function getCoordinates(Request $request) 
-  {
-    if ($request->has('zip')) {
-      $location = Location::where('zip', '=', $request->zip)
-        ->get();
-      $coords['lat'] = $location[0]->lat;
-      $coords['lon'] = $location[0]->lon;
+    /**
+     * Get all aliases for a specialty.
+     *
+     * @param string $specialtyCode - the parent specialty code (ex.: 'AJI')
+     * @return array of IDs
+     *    ex.: [80, 5, 35]
+     */
+    private function getAliases($specialtyCode)
+    {
+      $aliasIds = DB::Table('specialty_alias')
+        ->addSelect('alias_id')
+        ->where('specialty_id', '=', $specialty)->get();
+    }
+
+    /**
+     * Find coordinates for a location, based on zip or city and state.
+     *
+     */
+    public function getCoordinates(Request $request) 
+    {
+      if ($request->has('zip')) {
+        $location = Location::where('zip', '=', $request->zip)
+          ->get();
+        $coords['lat'] = $location[0]->lat;
+        $coords['lon'] = $location[0]->lon;
+        return $coords;
+      }
+
+      $location = Location::where('city', '=', $request->city)
+        ->where('state', '=', $request->state)
+        ->first();
+      $coords['lat'] = $location->lat;
+      $coords['lon'] = $location->lon;
       return $coords;
     }
 
-    $location = Location::where('city', '=', $request->city)
-      ->where('state', '=', $request->state)
-      ->first();
-    $coords['lat'] = $location->lat;
-    $coords['lon'] = $location->lon;
-    return $coords;
-  }
-  
-  public function getZip($city, $state) 
-  {
-    return Location::where('city', '=', $city)
-    ->where('state', '=', $state)
-    ->first()->zip;
-  }
+    public function getZip($city, $state) 
+    {
+      return Location::where('city', '=', $city)
+        ->where('state', '=', $state)
+        ->first()->zip;
+    }
 
-  /**
-   * Determine whether a specialty is a parent specialty or a subspecialty.
-   *
-   * @param Specialty
-   * @return boolean
-   */
-  private function isParentSpecialty(Specialty $specialty)
-  {
-    $result = DB::table('specialty_subspecialty')
-      ->where('specialty_id', '=', $specialty->code)
-      ->get();
-    
-    return !empty($result);
-  }
+    /**
+     * Determine whether a specialty is a parent specialty or a subspecialty.
+     *
+     * @param Specialty
+     * @return boolean
+     */
+    private function isParentSpecialty(Specialty $specialty)
+    {
+      $result = DB::table('specialty_subspecialty')
+        ->where('specialty_id', '=', $specialty->code)
+        ->get();
+
+      return !empty($result);
+    }
 }
