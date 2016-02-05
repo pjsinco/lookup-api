25b8ed72f8163468ee23458fa1d24bff229a23c6
pjsinco <psinco@osteopathic.org>
2016-02-05 07:26:04 -0600

    Fix method assignment for show route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,7 +21,7 @@ Route::group(['prefix' => 'api/v1'], function() {
      *
      */
     Route::get('doctors/search', 'DoctorController@search');
-    Route::get('doctors/{id}', 'DoctorController@search');
+    Route::get('doctors/{id}', 'DoctorController@show');
 
     /**
      * Specialties
